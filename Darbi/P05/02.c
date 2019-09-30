c161753d4963c7f90e0c83f2c6c74038267fa97b
user <koziskey@inbox.lv>
2019-09-30 16:14:44 +0300

    https://github.com/KozisKey/RTR105

---
+++ Darbi/P05/02.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+
+
+int main () 
+{
+	long a = 50000;//50,000
+	long b = 1000000;//1,000,000
+	long c = a * b;//kaadsirsgaidaamsrezultaats?
+	printf ("int datu tipa iz mers ir: %zu baiti \n",sizeof (int));
+	printf ("Apreekjina maunu reizinaajumu: \n");
+	printf ("a= %ld, b= %ld \n", a, b);
+	printf ("c=a*b= %ld*%ld = %ld \n", a,b,c );//rrezultaatsuzekraana
+}
+
---
+++ Darbi/readme.txt
@@ -0,0 +1,6 @@
+Klūda!
+
+
+. ./01.c: line 3: syntax error near unexpected token `('
+./01.c: line 3: `int main() {'
+
